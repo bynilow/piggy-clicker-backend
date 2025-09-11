10f3a6df922692d239ab1184df6a1e9746f8d7a6
bynilow <bynilow@mail.ru>
2025-09-11 21:59:26 +0500

    added origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://yourdomain.com', '*'],
+    origin: ['http://localhost:3000', 'https://piggy-clicker.vercel.app/', '*'],
     credentials: false
 }));
 
