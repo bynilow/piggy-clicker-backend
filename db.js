0b1016489fc998e8d912623797a3bf0ec4e5e889
bynilow <bynilow@mail.ru>
2025-09-11 14:01:53 +0500

    added env

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 import { Pool } from 'pg';
 
+const database = process.env.PG_DATABASE;
+const user = process.env.PG_USER;
+const password = process.env.PG_PASSWORD;
+const host = process.env.PG_HOST;
+const port = process.env.PG_PORT;
+
 const pool = new Pool({
-    user: "postgres",
-    password: "1234",
-    host: "localhost",
-    port: 5432,
-    database: "piggy_db"
+    user,
+    password,
+    host,
+    port,
+    database
 });
 
 export { pool };
