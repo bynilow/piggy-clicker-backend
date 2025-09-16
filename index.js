f1a727b77378161aaa8041c0573917fb941b9228
bynilow <bynilow@mail.ru>
2025-09-16 23:37:56 +0500

    added tg bot

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import cookieParser from 'cookie-parser';
 import { UserController } from './controller/user.controller.js';
 import { referralsRouter } from './routes/referrals.routes.js';
 
+import { Telegraf } from 'telegraf';
+
+// -------------------- EXPRESS --------------------
+
 const PORT = 8080;
 
 const app = express();
@@ -40,4 +44,37 @@ app.use('/api', coinsRouter);
 app.use('/api', boostsRouter);
 app.use('/api', referralsRouter);
 
-app.listen(PORT, () => console.log(`server starts on ${PORT} port`))
+app.listen(PORT, () => console.log(`server starts on ${PORT} port`));
+
+// -------------------- TG --------------------
+
+const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
+
+if (!TG_BOT_TOKEN) {
+    console.log("Токен бота не найден");
+};
+
+const tgBot = new Telegraf(TG_BOT_TOKEN);
+
+tgBot.start(async (ctx) => {
+    const payload = ctx.startPayload; // то, что приходит после ?start=
+
+    await ctx.reply(
+        "Добро пожаловать в Piggy Clicker 🐷!",
+        {
+            reply_markup: {
+                keyboard: [
+                    [
+                        { text: "🚀 Открыть приложение", web_app: { url: `https://t.me/PiggyClickerBot/?startapp=${payload}` } }
+                    ]
+                ],
+                resize_keyboard: true,
+            },
+        }
+    );
+});
+
+// запуск бота (long polling)
+tgBot.launch()
+    .then(() => console.log("Бот запущен"))
+    .catch((err) => console.error("Ошибка запуска бота:", err));
